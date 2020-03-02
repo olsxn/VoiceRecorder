fe12f87737af448e3a7caaa7e5ffc0f885599dcd
Halil Olsen <olsenhalil@gmail.com>
2020-03-02 20:32:31 +0000

    commit #2

--- app/src/main/java/com/voicerecorderproject/settings.java
+++ app/src/main/java/com/voicerecorderproject/settings.java
@@ -3,13 +3,16 @@ package com.voicerecorderproject;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.SimpleAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class settings extends AppCompatActivity {
 
@@ -24,29 +27,85 @@ public class settings extends AppCompatActivity {
 
     public void handleListView() {
         listView = findViewById(R.id.list_view);
-        ArrayList<String> arrayList = new ArrayList<>();
 
-        addArrayItems(arrayList);
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
-        listView.setAdapter(arrayAdapter);
+        // let items have context menus
+        registerForContextMenu(listView);
 
+        HashMap items = createHashMap();
+
+        List<HashMap<String, String>> listItems = new ArrayList<>();
+
+        // set up format
+        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.list_item,
+                new String[]{"First line", "Second line"},
+                new int[]{R.id.textView_items, R.id.textView_subItems});
+
+        Iterator it = items.entrySet().iterator();
+
+        // go through hashMap and add the strings to new hashMap resultsMap
+        while (it.hasNext()) {
+            HashMap<String, String> resultsMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry) it.next();
+            resultsMap.put("First line", pair.getKey().toString());
+            resultsMap.put("Second line", pair.getValue().toString());
+            listItems.add(resultsMap);
+        }
+
+        // set adapter to listView
+        listView.setAdapter(adapter);
+
+        // when an item in the list is clicked, call handleChoice
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(settings.this, "Clicked item " + (position + 1), Toast.LENGTH_SHORT).show();
+                handleChoice(position);
             }
         });
     }
 
-    public void addArrayItems(ArrayList<String> arrayList) {
-        arrayList.add("Audio Codec");
-        arrayList.add("Sample Rate");
-        arrayList.add("Max File Size");
-        arrayList.add("Keep screen awake");
-        arrayList.add("Do not disturb");
-        arrayList.add("Recording location");
+    public HashMap createHashMap() {
+        HashMap<String, String> nameDescription = new HashMap<>();
+        nameDescription.put("Audio codec", "Change the audio codec used for recordings");
+        nameDescription.put("Sample rate", "Set the sample rate used for recordings");
+        nameDescription.put("Max file size", "Set the max file size");
+        nameDescription.put("Keep screen awake", "Keep the screen awake while recording");
+        nameDescription.put("Do not disturb", "Set the phone to do not disturb while recording");
+        nameDescription.put("Recording location", "CHANGE THE RECORDING LOCATION");
+        return nameDescription;
     }
 
+    public void handleChoice(int position) {
+        switch (position) {
+            // audio codec
+            case 0:
+                // in here, bring up options of different audio codecs which the user can choose
+                break;
 
+            // sample rate
+            case 1:
+                // in here, bring sample rate options up
+                break;
 
+            // max file size
+            case 2:
+                // in here, create slider to decide max file size
+                break;
+
+            // keep screen awake
+            case 3:
+                // create button to select yes or no
+                break;
+
+            // do not disturb
+            case 4:
+                // same as above
+                break;
+
+            // recording location
+            case 5:
+                // change recording location
+                break;
+
+        }
+    }
 }
